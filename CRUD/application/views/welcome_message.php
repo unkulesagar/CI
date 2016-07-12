06f0b20af6e11a0718f5567b21ff3fb590cbea2c
unkulesagar <unkule.sagar@gmail.com>
2016-07-12 18:43:44 +0530

    7/122016 6:43

--- CRUD/application/views/welcome_message.php
+++ CRUD/application/views/welcome_message.php
@@ -83,6 +83,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 		<p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
 		<p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
+		<p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
